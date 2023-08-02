83624c1b352d0681d0b858bf6904c52e6f6ff4d8
ttak0422 <ttak0422@gmail.com>
2023-08-03 01:11:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ddc-sorter_reverse
+reverse sorter for ddc.vim
+
+### Required
+
+- https://github.com/vim-denops/denops.vim
+- https://github.com/Shougo/ddc.vim
---
+++ denops/@ddc-filters/sorter_reverse.ts
@@ -0,0 +1,13 @@
+import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.9.0/types.ts";
+import { FilterArguments } from "https://deno.land/x/ddc_vim@v3.9.0/base/filter.ts";
+
+type Params = { [key: string]: never };
+
+export class Filter extends BaseFilter<Params> {
+  override filter({ items }: FilterArguments<Params>): Promise<Item[]> {
+    return Promise.resolve([...items].reverse());
+  }v
+  override params(): Params {
+    return {};
+  }
+}
